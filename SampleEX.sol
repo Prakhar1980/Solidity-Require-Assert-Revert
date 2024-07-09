b0ae88a05c9338cb73720022182c4b6dcb4b89a9
Prakhar1980 <150064371+Prakhar1980@users.noreply.github.com>
2024-07-09 22:13:42 +0530

    Update SampleEX.sol

--- SampleEX.sol
+++ SampleEX.sol
@@ -2,19 +2,32 @@
 pragma solidity ^0.8.26;
 
 contract ReqAssRev {
-    // Function to demonstrate the use of require()
+    error AmountTooLow(uint256 amount);
+    error InvalidResult();
+    error AlwaysReverts();
+
+    // Function to demonstrate the use of require() 
     function requireEx(uint256 _amount) external pure {
-        require(_amount > 10, "Amount must be greater than 10");
+        // Check if the amount is greater than 10, otherwise revert with custom error
+        if (_amount <= 10) {
+            revert AmountTooLow(_amount);
+        }
     }
 
-    // Function to demonstrate the use of assert()
+    // Function to demonstrate the use of assert() 
     function assertEx(uint256 _value) external pure {
-        uint256 RES = _value/2;
-                assert(RES != 0);
+
+        uint256 RES = _value / 2;
+
+        if (RES == 0) {
+            revert InvalidResult();
+        }
     }
 
-    // Function to demonstrate the use of revert()
-    function alwaysRevert(uint256 ) external pure {
-        revert("This function always reverts");
+    // Function to demonstrate the use of revert() 
+    function alwaysRevert(uint256) external pure {
+
+        revert AlwaysReverts();
     }
 }
+
